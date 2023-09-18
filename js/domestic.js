1c95f89a69f90eb208b620ceaa0eb11fa78698a1
ogon38 <142369839+ogon38@users.noreply.github.com>
2023-09-18 13:20:56 +0900

    Add files via upload

---
+++ js/domestic.js
@@ -0,0 +1,67 @@
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+    $('html, body').animate({
+    scrollTop: target.offset().top
+    }, 500);//움직이는 시간 조정
+    return false;
+    }
+    }
+    });
+    });
+
+
+    const remainTimes = document.querySelectorAll(".time_text #remain-time");
+
+    function updateDiffTime() {
+        remainTimes.forEach((remainTime, index) => {
+            // 여기에서 각 요소에 대한 서로 다른 시간을 계산하고 설정합니다.
+            const masTimes = ["2023-09-20", "2023-09-23", "2023-09-25"]; // 각 요소에 대한 날짜를 배열로 설정
+    
+            const masTime = new Date(masTimes[index]); // 각 요소에 대한 날짜 설정
+            const todayTime = new Date();
+    
+            const diff = masTime - todayTime;
+    
+            const diffDay = String(Math.floor(diff / (1000 * 60 * 60 * 24)));
+            const diffHour = String(Math.floor((diff / (1000 * 60 * 60)) % 24)).padStart(2, "0");
+            const diffMin = String(Math.floor((diff / (1000 * 60)) % 60)).padStart(2, "0");
+            const diffSec = String(Math.floor(diff / 1000 % 60)).padStart(2, "0");
+    
+            remainTime.innerText = `${diffDay}일 ${diffHour}:${diffMin}:${diffSec}`;
+        });
+    }
+    
+    updateDiffTime();
+    setInterval(updateDiffTime, 1000);
+        
+        
+    
+    document.addEventListener('DOMContentLoaded', function () {
+        const tabs = document.querySelectorAll('.season_tab');
+        
+        tabs.forEach(tab => {
+            tab.addEventListener('click', function () {
+                // 현재 클릭한 탭을 활성화 상태로 변경
+                tabs.forEach(t => t.classList.remove('active'));
+                tab.classList.add('active');
+                
+                // 현재 활성화된 탭에 따라 콘텐츠를 표시/숨김 처리
+                const tabId = tab.querySelector('input').id;
+                const content = document.querySelector(`#${tabId}-content`);
+                
+                const allContents = document.querySelectorAll('.season_content');
+                allContents.forEach(c => c.style.display = 'none');
+                
+                if (content) {
+                    content.style.display = 'block';
+                }
+            });
+        });
+    });
+    
+    
+    
---
+++ js/oversea.js
@@ -0,0 +1,113 @@
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 70,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+
+
+
+
+  $(document).ready(function() {
+
+      //처음들어갔을때 모습 만들기...
+      $(".tab_content").hide(); //tab_content 부분을 모두 숨김.
+      $("ul.tabs li:first").addClass("active").show(); //첫번째 탭이 active클래스를 붙이고, 보여줌.
+      $(".tab_content:first").show(); //tab_contents중 첫번째꺼를 보여줌
+
+      //On Click Event
+      $("ul.tabs li").click(function() {
+
+          $("ul.tabs li").removeClass("active"); //Remove any "active" class
+          $(this).addClass("active"); //선택한 탭에만 active클래스 붙임
+          $(".tab_content").hide(); //모든 contents들 숨김
+
+          var activeTab = $(this).find("span").attr("data"); //Find the href attribute value to identify the active tab + content
+          /*.attr( attributeName ) -> 선택한 요소의 속성의 값을 가져옵니다.
+          .find()는 어떤 요소의 하위 요소 중 특정 요소를 찾을 때 사용합니다.*/
+          $(activeTab).fadeIn(); //Fade in the active ID content(active되어있는 칸의 id 값을 가진 부분이 나타남)
+          return false;
+      });
+
+  });
+
+
+
+
+  
+
+
+  $(function(){
+      //이미지 변경 함수 호출
+
+      $(".wrap_1").mouseenter(function(){
+          $(".cruise_more img").css({'transform':'translateX(10px)'});
+          
+          
+      });
+      $(".wrap_1").mouseleave(function(){
+          $(".cruise_more img").css({'transform':'translateX(0px)'});
+      });
+
+      $(".wrap_2").mouseenter(function(){
+          $(".imgClass").attr("src", "images/oversea/ship1.jpg");//변경될 이미지 주소를 넣어주세요
+          $(".layer2").css({'opacity':'0'});
+          $(".ship1").css({'transform':'translateY(-30px)'});
+      });
+      $(".wrap_2").mouseleave(function(){
+          $(".imgClass").attr("src", "images/oversea/ship1.jpg");//변경될 이미지 주소를 넣어주세요
+          $(".layer2").css({'opacity':'0.7'});
+          $(".ship1").css({'transform':'translateY(0px)'});
+      });
+
+      $(".wrap_3").mouseenter(function(){
+          $(".imgClass").attr("src", "images/oversea/ship2.jpg");//변경될 이미지 주소를 넣어주세요
+          $(".layer3").css({'opacity':'0'});
+          $(".ship2").css({'transform':'translateY(-30px)'});
+      });
+      $(".wrap_3").mouseleave(function(){
+          $(".imgClass").attr("src", "images/oversea/ship2.jpg");//변경될 이미지 주소를 넣어주세요
+          $(".layer3").css({'opacity':'0.7'});
+          $(".ship2").css({'transform':'translateY(0px)'});
+      });
+
+      $(".wrap_4").mouseenter(function(){
+          $(".imgClass").attr("src", "images/oversea/ship3.jpg");//변경될 이미지 주소를 넣어주세요
+          $(".layer4").css({'opacity':'0'});
+          $(".ship3").css({'transform':'translateY(-30px)'});
+      });
+      $(".wrap_4").mouseleave(function(){
+          $(".imgClass").attr("src", "images/oversea/ship3.jpg");//변경될 이미지 주소를 넣어주세요
+          $(".layer4").css({'opacity':'0.7'});
+          $(".ship3").css({'transform':'translateY(0px)'});
+      });
+    
+  });
+
+
+
+  $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+      $('html, body').animate({
+      scrollTop: target.offset().top
+      }, 500);//움직이는 시간 조정
+      return false;
+      }
+      }
+      });
+      });
